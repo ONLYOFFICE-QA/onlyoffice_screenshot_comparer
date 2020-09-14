d19c7a6ed4a0fc6a96ade55cf10ca0e5347e139b
Pavel Lobashov <shockwavenn@gmail.com>
2020-09-14 12:53:41 +0300

    Fix more

--- spec/onlyoffice_screenshot_comparer/compare_result_spec.rb
+++ spec/onlyoffice_screenshot_comparer/compare_result_spec.rb
@@ -6,12 +6,12 @@ describe 'CompareResult' do
   it 'CompareResult#same? return true for same images' do
     data1 = OnlyofficeScreenshotComparer::FileScreenshotList.new('spec/examples/lost_chart/doc_with_chart')
     data2 = OnlyofficeScreenshotComparer::FileScreenshotList.new('spec/examples/lost_chart/doc_with_chart')
-    expect(data1.compare(data2).same?).to be_truthy
+    expect(data1.compare(data2)).to be_same
   end
 
   it 'CompareResult#same? return false for not same images' do
     data1 = OnlyofficeScreenshotComparer::FileScreenshotList.new('spec/examples/lost_chart/doc_with_chart')
     data2 = OnlyofficeScreenshotComparer::FileScreenshotList.new('spec/examples/lost_chart/doc_without_chart')
-    expect(data1.compare(data2).same?).to be_falsey
+    expect(data1.compare(data2)).not_to be_same
   end
 end
